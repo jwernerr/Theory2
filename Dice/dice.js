a19f53ac9c082073f495bfe3b49d00d71d9fea9e
jenny <jenny.w0400@gmail.com>
2024-12-04 05:31:04 +0100

    aaaaaa

--- Dice/dice.js
+++ Dice/dice.js
@@ -3,28 +3,5 @@ let sides = 6;
 let dice = 2;
 let probability = 0.3;
 let pspace = [];
-for (let i = 1; i <= (sides ** dice); i++) {
-    let step = (sides ** dice) % i;
-    let round = (sides ** dice) / (i - step);
-    let next = [];
-    for (let j = 0; j < dice; j++) {
-        next.push(1);
-    }
-    let last = next;
-    if (i === 1) {
-        pspace.push(next);
-    }
-    else {
-        for (let j = dice - 1; j >= 0; j--) {
-            if (last[j] = sides) {
-                next[j] = 1;
-            }
-            else {
-                next[j] = last[j] + 1;
-            }
-        }
-        pspace.push(next);
-        last = next;
-    }
+for (let i = 1; i <= (sides ** dice) - 1; i++) {
 }
-console.log(pspace);
--- Dice/dice.ts
+++ Dice/dice.ts
@@ -3,31 +3,8 @@ let dice: number = 2;
 let probability: number = 0.3;
 
 let pspace: number[][] = []
-for (let i: number = 1; i <= (sides ** dice); i++) {
-    let step: number = (sides ** dice) % i;
-    let round: number = (sides ** dice) / (i - step);
 
-    let next: number[] = []
-    for (let j: number = 0; j < dice; j++) {
-        next.push(1)
-    }
-    let last: number[] = next;
 
-    if (i === 1) {
-        pspace.push(next)
-    }
-    else {
-        for (let j: number = dice - 1; j >= 0; j--) {
-            if (last[j] = sides) {
-                next[j] = 1
-            }
-            else {
-                next[j] = last[j] + 1
-            }
-        }
-        pspace.push(next);
-        last = next;
-    }
+for (let i: number = 1; i <= (sides ** dice) - 1; i++) {
+
 }
-
-console.log(pspace);
